c03d0ffae176cc371017393960cba399ebad2f7c
Petru Birgoveanu <Petru.Birgoveanu@ibm.com>
2018-12-05 15:20:46 +0200

    PB: Extending framework functionality with basic GET Tests for Response Headers

--- src/test/java/ResponseHeaders.java
+++ src/test/java/ResponseHeaders.java
@@ -1,16 +1,21 @@
+import main.java.ResponseUtils;
 import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class ResponseHeaders extends BaseClass{
 
@@ -46,4 +51,43 @@ public class ResponseHeaders extends BaseClass{
 
 
     }
+    
+    @Test
+    public void serverIsGithub() throws IOException {
+
+        HttpGet get = new HttpGet(BASE_ENDPOINT );
+
+        response = client.execute(get);
+
+        String headerValue = ResponseUtils.getHeader(response, "Server");
+
+        Assert.assertEquals(headerValue, "GitHub.com");
+
+
+    }
+
+    @Test
+    public void xRateLimitIsSixty() throws IOException {
+
+        HttpGet get = new HttpGet(BASE_ENDPOINT );
+
+        response = client.execute(get);
+
+        String limitVal = ResponseUtils.getHeaderjava8Way(response, "X-RateLimit-Limit");
+        assertEquals(limitVal, "60");
+
+    }
+
+    @Test
+    public void eTagIsPresent() throws IOException {
+
+        HttpGet get = new HttpGet(BASE_ENDPOINT );
+
+        response = client.execute(get);
+
+        boolean tagIsPreent = ResponseUtils.headerIsPresent(response, "ETag");
+        assertTrue(tagIsPreent);
+
+    }
+
 }
